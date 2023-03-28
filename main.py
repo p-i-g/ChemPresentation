931178c22db70660f29f4585b05df49e4bf9fc44
road-pig <qiuzimiao@gmail.com>
2023-03-28 19:43:19 +0800

    make animations in mma

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
-from manim import *
+from chanim import *
 # or: from manimlib import *
 from manim_slides.slide import Slide
+from scipy.special import sph_harm
+from manim_meshes.models.manim_models.opengl_mesh import FastManimMesh
+from manim_meshes.models.data_models.mesh import Mesh
+from trimesh import load_mesh
 
 
-class BasicExample(Slide):
+class Introduction(Slide):
     def construct(self):
-        circle = Circle(radius=3, color=BLUE)
-        dot = Dot()
+        # Introduction
+        title = Text('OLEDs')
+        self.play(Create(title))
+        self.next_slide()
 
-        self.play(GrowFromCenter(circle))
-        self.next_slide()  # Waits user to press continue to go to the next slide
+        expanded_title = Text('Organic Light Emitting Diodes')
+        self.play(Transform(title, expanded_title))
 
-        self.start_loop()  # Start loop
-        self.play(MoveAlongPath(dot, circle), run_time=2, rate_func=linear)
-        self.end_loop()  # This will loop until user inputs a key
 
-        self.play(dot.animate.move_to(ORIGIN))
-        self.next_slide()  # Waits user to press continue to go to the next slide
+class Test(Slide):
+    def construct(self):
+        tmesh = load_mesh('test.obj', file_type='obj')
+        print("test")
+
+        mesh = FastManimMesh(mesh=Mesh(tmesh.vertices, tmesh.faces))
+        print("test")
+        self.add(mesh)
+
+
+class Benzene(Slide):
+    def construct(self):
+        chem = ChemWithName(r'*6(-=-=-=)', "Caffeine")
+        chem.scale(0.1)
+
+        self.play(chem.creation_anim())
+
+        self.wait()
+        # template = TexTemplate()
+        # template.add_to_preamble(r'\usepackage{chemfig}')
+        # chem = Tex(r"\chemfig{*6((=O)-N(-CH_3)-*5(-N=-N(-CH_3)-=)--(=O)-N(-H_3C)-)}", tex_template=template)
+        #
+        # self.play(Create(chem))
+        # self.wait()
+
+
+class SphericalHarmonicSurface:
+    def __init__(self, l, m):
+        self.l = l
+        self.m = m
